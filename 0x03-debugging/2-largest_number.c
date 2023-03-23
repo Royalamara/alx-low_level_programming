a8a8cfadd1246332bd24da8e7ad5949ec3386014
Royalamara <royalamara01@gmail.com>
2023-03-23 09:03:32 -0700

    looking fot the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,11 +13,10 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
-
 	{
 		largest = a;
 	}
-	else if (b > c)
+	else if (b > c )
 	{
 		largest = b;
 	}
@@ -25,5 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+	
 	return (largest);
 }
